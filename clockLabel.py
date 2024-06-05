9e93b13a96fb31d472ca04040b714c6bea0799ff
Filip Bergman <filbe169@student.liu.se>
2024-06-05 16:31:13 +0200

    clock works

--- clockLabel.py
+++ clockLabel.py
@@ -1,13 +1,21 @@
 import tkinter as tk
+from time import localtime, strftime
+
 
 class ClockLabel(tk.Label):
 
     def __init__(self, master):
         super().__init__(master)
+        self.master = master
         self.text = tk.StringVar()
         self.configure(bg="black", fg="white", textvariable=self.text)
-        self.text.set("test")
+        self.update_time()
         
+    def update_time(self):
+        self.text.set(strftime("%H:%M:%S", localtime()))
+        self.master.after(100, self.update_time)
+        
+
     
     
 
