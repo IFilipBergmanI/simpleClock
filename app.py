f9c6c5926dc13d382c7a20d322627767842e6059
Filip Bergman <filbe169@student.liu.se>
2024-06-05 17:51:21 +0200

    added window icon and name

--- app.py
+++ app.py
@@ -1,17 +1,35 @@
+import os
+
 import tkinter as tk
 from clockLabel import ClockLabel
+
 
 class App:
 
     def __init__(self):
         self.root = tk.Tk()
-        self.frame = tk.Frame(self.root)
 
-        self.frame.configure(background="black", width=200, height=200)
-        self.frame.pack(fill=tk.BOTH, expand=True)
+        # Give the window a name and an icon
+        self.root.title("Clock")
+        self.setup_window_icon()
 
-        self.clock = ClockLabel(self.frame)
-        self.clock.pack()
+        # Set size, resizability and bg color
+        self.root.geometry("200x200")
+        self.root.resizable(False, False)
+        self.root.configure(bg="black")
+
+        # Create a clock and include it
+        self.clock = ClockLabel(self.root)
+        self.clock.pack(expand=True)
+
+    def setup_window_icon(self):
+        # Find image path
+        basedir = os.path.dirname(__file__)
+        path_to_icon = os.path.join(basedir, os.path.join("images", "clock.png"))
+
+        # Set the icon
+        img = tk.PhotoImage(file=path_to_icon)
+        self.root.iconphoto(False, img)
 
     def run(self):
         self.root.mainloop()
--- clockLabel.py
+++ clockLabel.py
@@ -8,11 +8,16 @@ class ClockLabel(tk.Label):
         super().__init__(master)
         self.master = master
         self.text = tk.StringVar()
-        self.configure(bg="black", fg="white", textvariable=self.text)
+        self.configure(bg="black", fg="white", textvariable=self.text, font=("Arial", 25))
+
+        # start updating the time, it is only required once as update time schedules itself to be called again
         self.update_time()
         
     def update_time(self):
+        # Update time text based on current local time
         self.text.set(strftime("%H:%M:%S", localtime()))
+
+        # Schedule next time update
         self.master.after(100, self.update_time)
         
 
